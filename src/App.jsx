150d6a1024b151d2bebb33b2dc034b984960841b
Prashant <dhimalprashant25@gmail.com>
2024-11-09 23:19:28 +0530

    changes in route of app.jsx and code added on pages page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Hero from './components/Hero/Hero'
-import Menu from './pages/Menu'
-import Login from './pages/Login'
-import Cart from './pages/Cart'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Hero from "./components/Hero/Hero";
+import Menu from "./pages/Menu";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import PalceOrder from "./pages/PalceOrder";
+import Orders from "./pages/Orders";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <Routes>
-        <Route path='/hero' element={<Hero/>} />
-        <Route path='/menu' element={<Menu/>}/>
-        <Route path='/login' element={<Login/>} />
-        <Route path='/cart' element={<Cart/>} />
-    </Routes>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/hero" element={<Hero />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/place-order" element={<PalceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/PalceOrder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PalceOrder() {
+  return (
+    <div>PalceOrder</div>
+  )
+}
+
+export default PalceOrder
