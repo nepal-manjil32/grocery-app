2ed6439aab84aea7a5007bc502cf6a517e7f3a74
Manjil Nepal <nepal.manjil3294@gmail.com>
2024-11-09 22:20:19 +0530

    new edits

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll';
+
+const navbar = () => {
+
+  const [sticky, setSticky] = useState(false);
+  useEffect(()=>{
+    window.addEventListener('scroll', ()=>{
+      window.scrollY > 80 ? setSticky(true) : setSticky(false);
+    })
+  },[])
+
+  return (
+    <nav className={`containe ${sticky ? 'darknav' : ''}`}>
+      <img src={logo} alt=""  className='logo'/>
+      <ul>
+        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+        <li><Link to='about' smooth={true} offset={-250} duration={500}>About</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-300} duration={500}>Shop</Link></li>
+        <li><Link to='contact' smooth={true} offset={-300} duration={500} className='btn'>Contact Us</Link></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default navbar
