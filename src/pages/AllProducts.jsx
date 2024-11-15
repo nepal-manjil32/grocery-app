e8219225a5071b16fcabfd43b054f3f8c703d94b
Prashant <dhimalprashant25@gmail.com>
2024-11-15 16:59:40 +0530

    tailwindcss framework added

---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/Shopcontext'
+
+const AllProducts = () => {
+
+    const {products} = useContext(ShopContext);
+    
+  return (
+    <div>
+        <div className=''>
+
+        </div>
+    </div>
+  )
+}
+
+export default AllProducts
