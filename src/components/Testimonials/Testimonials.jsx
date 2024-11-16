83c4a95e8970349c4ff2423bc2fc204209653af6
Jayash Shrestha <jaayaash.com>
2024-11-16 11:29:16 +0530

    testimonials edited

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,10 +2,11 @@ import React, { useRef } from 'react'
 import './Testimonials.css'
 import next_icon from '../../assets/next-icon.png'
 import back_icon from '../../assets/back-icon.png'
-import user_1 from '../../assets/manjil.png'
+import user_0 from '../../assets/manjil.png'
+import user_1 from '../../assets/prayash.jpg'
 import user_2 from '../../assets/jayash.png'
 import user_3 from '../../assets/sunit-pro.png'
-import user_4 from '../../assets/prashant.png'
+import user_4 from '../../assets/prashant-pro.jpg'
 
 const Testimonials = () => {
   const slider = useRef(); // to access the user tag
@@ -31,6 +32,21 @@ const Testimonials = () => {
       <img src={back_icon} alt="" className='back-btn' onClick={slideBackward}/>
       <div className="slider">
           <ul ref={slider}>
+            <li>
+              <div className="slide first-user">
+                <div className="user-info">
+                  <img src={user_0} alt="" />
+                  <div>
+                    <h3>Manjil Nepal</h3>
+                    <span>Bhadrapur, Jhapa</span>
+                  </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Odio tempore vitae ipsa mollitia porro nobis, veniam consequatur 
+                  aspernatur, repellat aliquid, ipsam ut accusamus aperiam quasi 
+                  error sequi architecto excepturi quia!</p>
+              </div>
+            </li>
             <li>
               <div className="slide first-user">
                 <div className="user-info">
