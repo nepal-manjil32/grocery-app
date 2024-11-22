d58a1581cb4a80b791c4a46ffe2eb973dcff4de4
Prashant <dhimalprashant25@gmail.com>
2024-11-22 21:56:33 +0530

    updated

--- src/components/InsertProducts/InsertProducts.jsx
+++ src/components/InsertProducts/InsertProducts.jsx
@@ -11,6 +11,7 @@ import barbecue from '../../assets/barbecue.png';
 import household from '../../assets/household.png';
 import rice from '../../assets/rice.png';
 import search from '../../assets/search.png';
+import snacks from '../../assets/snack.png';
 
 const categoryLogos = {
   vegetables: vegetables,
@@ -20,6 +21,7 @@ const categoryLogos = {
   meat: barbecue,
   beverages: drinks,
   puja: puja,
+  snacks: snacks,
 };
 
 const InsertProducts = () => {
@@ -33,6 +35,7 @@ const InsertProducts = () => {
 
   const sectionRefs = {
     vegetables: useRef(null),
+    snacks: useRef(null),
     grains: useRef(null),
     bakery: useRef(null),
     household: useRef(null),
@@ -153,7 +156,7 @@ const InsertProducts = () => {
     dynamicSubTitle = `Found ${filteredProducts.length} matching product(s)`;
     dynamicLogo =
       filteredProducts.length > 0
-        ? categoryLogos[filteredProducts[0]?.category] || puja
+        ? categoryLogos[filteredProducts[0]?.category]
         : search;
   }
 
@@ -228,6 +231,12 @@ const InsertProducts = () => {
         'खाद्य, अन्न, र तेल',
         rice,
         'grains'
+      )}
+      {renderProductsSection(
+        'Snacks and Branded Foods',
+        'नास्ता र ब्रान्डेड खाद्य पदार्थहरू',
+        snacks,
+        'snacks'
       )}
       {renderProductsSection(
         'Bakery Cakes Dairy',
