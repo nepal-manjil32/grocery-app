ce442160d0cc42ce0e201f4a92f14328741decd4
Prashant <dhimalprashant25@gmail.com>
2024-11-28 09:59:01 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import InsertProducts from '../components/InsertProducts/InsertProducts';
 import AfterLogNav from '../components/Navbr_inside/AfterLogNav';
 import Carousel from '../components/Carousel/Carousel';
 import BottomCarousel from '../components/BottomCarousel/BottomCarousel';
-// import AfterLogFooter from '../components/AfterLogFooter/AfterLogFooter';
 import Footer from '../components/Footer/Footer';
 
 const Home = () => {
